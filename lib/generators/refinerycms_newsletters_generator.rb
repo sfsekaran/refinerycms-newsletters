96c176614e3b55708c649f6f576c8cf97cca56a4
Sathya Sekaran <sfsekaran@gmail.com>
2011-08-25 14:25:54 -0700

    Making sure the generator works properly.

--- lib/generators/refinerycms_newsletters_generator.rb
+++ lib/generators/refinerycms_newsletters_generator.rb
@@ -1,4 +1,6 @@
-class RefinerycmsNewsletters < Refinery::Generators::EngineInstaller
+require 'refinery/generators'
+
+class RefinerycmsNewslettersGenerator < Refinery::Generators::EngineInstaller
 
   source_root File.expand_path('../../../', __FILE__)
   engine_name "newsletters"
